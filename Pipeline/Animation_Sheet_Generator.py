187d5e0673809f678fec962ad6023e6974b9c568
colpiche <114240773+colpiche@users.noreply.github.com>
2024-04-17 16:34:25 +0200

    Animation sheets are now arrays

--- Pipeline/Animation_Sheet_Generator.py
+++ Pipeline/Animation_Sheet_Generator.py
@@ -2,30 +2,35 @@ from PIL import Image
 import glob
 import os
 
-# FOLDERS AND FILES PARAMETERS
-SAMPLES_DIR_FACE: str = r"Assets\Sprites\Character\Rat\Rat_Face_statique\Face_STATIQUE"
-SAMPLES_DIR_PROFILE: str = r"Assets\Sprites\Character\Rat\Rat_Profil_statique\Profil_statique"
-SAMPLES_DIR_BACK: str = r"Assets\Sprites\Character\Rat\Rat_Dos_statique\Dos_statique"
-DIRECTORIES_TO_PARSE: list[str] = [SAMPLES_DIR_FACE, SAMPLES_DIR_PROFILE, SAMPLES_DIR_BACK]
-OUTPUT_DIR: str = r"Assets\Sprites\Character\Rat"
-OUTPUT_FILE: str = r"Rat_AnimationSheet.png"
 
-# IMAGE PARAMETERS
+# ======================== FOLDERS AND FILES PARAMETERS =======================
+
+
+DIRECTORIES_TO_PARSE: list[str] = [
+        r"Assets\Sprites\Character\Rat\Dos",
+        r"Assets\Sprites\Character\Rat\Face",
+        r"Assets\Sprites\Character\Rat\Profil"
+    ]
+OUTPUT_DIR: str = r"Assets\Sprites\Character\Rat"
+OUTPUT_FILES_NAME: str = "Rat_AnimationSheet"
+OUTPUT_EXTENSION: str = "png"
+
+
+# ============================== IMAGE PARAMETERS =============================
+
+
 CROP: bool = True
-CROP_COORDS: tuple[int, int, int, int] = (370, 380, 1730, 1020)  # (left, upper, right, lower)
+
+# (left, upper, right, lower)
+CROP_COORDS: tuple[int, int, int, int] = (370, 380, 1730, 1020)  # Rat
+
 RESIZE: bool = True
 RESIZE_WIDTH: int = 600
 
-images: list[str] = []
-output_image: Image.Image
-mode: str
+TILES_PER_ROW: int = 8
 
 
-def concat_images(image1: Image.Image, image2: Image.Image) -> Image.Image:
-    concat_image: Image.Image = Image.new(mode, (image1.width + image2.width, image1.height))
-    concat_image.paste(image1, (0, 0))
-    concat_image.paste(image2, (image1.width, 0))
-    return concat_image
+# ============================ FUNCTIONS DEFINITION ===========================
 
 
 def open_image(file: str) -> Image.Image:
@@ -38,27 +43,83 @@ def open_image(file: str) -> Image.Image:
         image = image.crop(CROP_COORDS)
 
     if RESIZE:
-        image = image.resize((RESIZE_WIDTH, int(image.height * (RESIZE_WIDTH / image.width))))
+        image = image.resize(
+            (
+                RESIZE_WIDTH,
+                int(image.height * (RESIZE_WIDTH / image.width))
+            )
+        )
 
     return image
+
+
+# ============================= CLASSES DEFINITION ============================
+
+
+class AnimationSheetGenerator:
+    _nb_rows: int
+    _concat_image_width: int
+    _concat_image_height: int
+    _mode: str
+    _concat_image: Image.Image
+
+    def __init__(self, nb_images: int, images_width: int, images_height: int, mode: str):
+
+        if nb_images % TILES_PER_ROW == 0:
+            self._nb_rows = nb_images // TILES_PER_ROW
+        else:
+            self._nb_rows = nb_images // TILES_PER_ROW + 1
+
+        self._concat_image_width = images_width * TILES_PER_ROW
+        self._concat_image_height = images_height * self._nb_rows
+        self._mode = mode
+        self._concat_image = Image.new(
+            mode,
+            (
+                self._concat_image_width,
+                self._concat_image_height
+            )
+        )
+
+    def add_image(self, image: Image.Image, index: int):
+        self._concat_image.paste(
+            image,
+            (
+                image.width * (index % TILES_PER_ROW),
+                image.height * (index // TILES_PER_ROW)
+            )
+        )
+
+    def get_result(self) -> Image.Image:
+        return self._concat_image
+
+
+# ==================================== MAIN ===================================
 
 
 if __name__ == "__main__":
 
     for directory in DIRECTORIES_TO_PARSE:
         files = glob.glob(directory + "/*.png")
-        images.extend(files)
+        sample_file: Image.Image = open_image(files[0])
 
-    for i in range(len(images) - 1):
-        print(f"Concatenating ${images[i + 1]}")
+        sheet: AnimationSheetGenerator = AnimationSheetGenerator(
+            len(files),
+            RESIZE_WIDTH,
+            int(RESIZE_WIDTH * (sample_file.height / sample_file.width)),
+            sample_file.mode
+        )
 
-        if i == 0:
-            output_image = open_image(images[i])
-            mode = output_image.mode
+        for i in range(len(files)):
+            print(f"Processing {files[i]}")
+            sheet.add_image(open_image(files[i]), i)
 
-        image2 = open_image(images[i + 1])
-        output_image = concat_images(output_image, image2)
+        filepath: str = os.path.join(
+            OUTPUT_DIR,
+            f"{OUTPUT_FILES_NAME}_{os.path.split(directory)[1]}.{OUTPUT_EXTENSION}"
+        )
 
-    print(f"Saving to {os.path.join(OUTPUT_DIR, OUTPUT_FILE)}")
-    output_image.save(os.path.join(OUTPUT_DIR, OUTPUT_FILE))
+        print(f"Saving to {filepath}")
+        sheet.get_result().save(filepath)
+
     print("Done")
